0dbfefb08465e5a624a4e0ad0f8715a32b0bd8be
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2020-11-02 16:30:11 +0800

    Add group response time plot code and output

--- CS424_MP1/YOLOv3/test_scheduler.py
+++ CS424_MP1/YOLOv3/test_scheduler.py
@@ -1,6 +1,6 @@
 import time
 from scheduling.Scheduler import *
-
+import matplotlib.pyplot as plt
 
 start = time.time()
 
@@ -15,8 +15,13 @@ print("Elapsed time: %f s" % (end - start))
 
 
 # # example for using visualize_history_file()
-# history = read_json_file("scheduling_history.json")
+history = read_json_file("scheduling_history.json")
 # visualize_history_file(history)
 # # calculate group average response time from history file
-# group_response_time = get_group_avg_response_time(history)
-# print(group_response_time)
+group_response_time = get_group_avg_response_time(history)
+x_axis = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-99']
+plt.bar(x_axis, group_response_time)
+plt.xlabel('Distance')
+plt.ylabel('Average Response Time')
+plt.title('Average Response Time of bbox')
+plt.savefig('group-response-time-plot')
